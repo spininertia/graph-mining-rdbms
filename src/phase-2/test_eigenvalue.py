111cf37c5a774dfdb36d41c078a24d6a1f5a5836
Wei Chen <ipondering.weic@gmail.com>
2013-11-02 18:10:46 -0400

    lanczos-no done

--- src/phase-2/test_eigenvalue.py
+++ src/phase-2/test_eigenvalue.py
@@ -13,22 +13,25 @@ def test_qeigen_quodratic(conn):
     create_vector_or_matrix(q, conn)
     create_vector_or_matrix(r, conn)
     cur = conn.cursor()
-    cur.execute("insert into %s values (0,0,0.5),(0,1,0.2),(0,2,0.3),(1,0,0.1),(1,1,0.7),(1,2,0.3),(2,0,0.1),(2,1,0.2),(2,2,0.5)" % A)
-    eigen_quodratic(A, q, r, 3, conn)
+    # cur.execute("insert into %s values (0,0,0.5),(0,1,0.2),(0,2,0.3),(1,0,0.1),(1,1,0.7),(1,2,0.3),(2,0,0.1),(2,1,0.2),(2,2,0.5)" % A)
+    cur.execute("insert into %s values (0,0,0.7),(0,1,0.2),(1,0,0.6),(1,1,0.1)" % A)    
+    eigen_quodratic(A, q, r, 2, conn)
 
 def test_lanczos(conn):
     b = 'b'
     v = 'v'    
+    dim = 3
+    create_vector_or_matrix(v, conn)
     clear_table("v", conn)
-    random_square_matrix(v, 10, conn);
-    # cur = conn.cursor()    
-    # cur.execute("insert into %s values (0,0,0.5),(0,1,0.2),(0,2,0.3),(1,0,0.1),(1,1,0.7),(1,2,0.3),(2,0,0.1),(2,1,0.2),(2,2,0.5)" % v)
+    # random_square_matrix(v, dim, conn);
+    cur = conn.cursor()    
+    cur.execute("insert into %s values (0,0,0.5),(0,1,0.2),(0,2,0.3),(1,0,0.1),(1,1,0.7),(1,2,0.3),(2,0,0.1),(2,1,0.2),(2,2,0.5)" % v)
     create_vector_or_matrix(b, conn)
-    random_vector(b, 10, conn)
-    # cur.execute("insert into %s values (0,0,0.1),(1,0,0.2),(2,0,0.4)" % b)    
-    lanczos(v, b, 10, 4, conn)    
+    # random_vector(b, dim, conn)
+    cur.execute("insert into %s values (0,0,0.1),(1,0,0.2),(2,0,0.4)" % b)    
+    lanczos(v, b, dim, dim, conn)    
 
 if __name__ == "__main__" :
     conn = psycopg2.connect(database="mydb", host="127.0.0.1")
-    test_lanczos(conn)
-    # test_qeigen_quodratic(conn)
+    # test_lanczos(conn)
+    test_qeigen_quodratic(conn)
