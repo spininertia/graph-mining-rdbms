28c5801c617cdc40c049956924e4a3da0cf57f28
Wei Chen <ipondering.weic@gmail.com>
2013-11-22 13:03:01 -0500

    add task 1 test & update doc

--- src/phase-3/src/test_degree_distribution.py
+++ src/phase-3/src/test_degree_distribution.py
@@ -23,6 +23,7 @@ class DegreeDistributionTest(unittest.TestCase):
         print "roadNet-CA....."
         undirect_dis(tbl_name, 'task1_roadnetca_result', self.conn)
 
+    @unittest.skip("")
     def test_wiki_talk(self):
         """http://snap.stanford.edu/data/wiki-Talk.html"""
         # directed
@@ -62,35 +63,3 @@ class DegreeDistributionTest(unittest.TestCase):
         result_tbl = "task1_youtube_result"
         load_undirected_graph_into_table(tbl_name, data_file, True, self.conn)
         undirect_dis(tbl_name, result_tbl, self.conn)
-
-# @time_it    
-# def test_route_view(conn):
-#     tbl_name = 'task1_edge_un_route'
-#     data_file = 'data/as20000102.txt'
-#     load_undirected_graph_into_table(tbl_name, data_file, True, conn)
-#     print "[degree distribution]testing route view"
-#     print "data loaded"
-#     undirect_dis(tbl_name, 'task1_un_route', conn)
-#     print "calculation done"
-
-# @time_it
-# def test_gplus(conn):
-#     tbl_name = 'task1_edge_gplus'
-#     data_file = 'data/gplus_directed.txt'
-#     load_undirected_graph_into_table(tbl_name, data_file, False, conn)
-#     print "[degree distribution]testing google plus"
-#     print "data loaded"
-#     indis(tbl_name, "task1_in_gplus", conn)
-#     outdis(tbl_name, "task1_out_gplus", conn)
-#     print "calculation done"
-
-# @time_it
-# def test_advogato(conn):
-#     tbl_name = 'task1_edge_advogato'
-#     data_file = 'data/advogato_undirected.txt'
-#     load_undirected_graph_into_table(tbl_name, data_file, False, conn)
-#     print "[degree distribution]testing advogato"
-#     print "data loaded"
-#     indis(tbl_name, "task1_in_advogato", conn)
-#     outdis(tbl_name, "task1_out_advogato", conn)
-#     print "calculation done"        
