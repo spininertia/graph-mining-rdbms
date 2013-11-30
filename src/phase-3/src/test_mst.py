0ff741f3d962f8e5a5d321c27f9a57b6f0506352
Siping <hotcoderj@gmail.com>
2013-11-30 14:26:22 -0500

    misc

---
+++ src/phase-3/src/test_mst.py
@@ -0,0 +1,19 @@
+import sys
+import time
+import psycopg2
+import unittest
+from mst.mst import *
+from graph_generator import *
+class MinimumSpanningTreeTest(unittest.TestCase):
+  def setUp(self):
+    self.conn = psycopg2.connect(database="mydb", host="127.0.0.1")    
+
+  def tearDown(self):
+    pass
+    
+  def test_runtime(self):
+    cur = self.conn.cursor()
+    gname = "mst_graph"
+    generate_undirected_graph(gname, 5000, self.conn)
+    print "graph builded"
+    mst(self.conn, gname, gname)		
